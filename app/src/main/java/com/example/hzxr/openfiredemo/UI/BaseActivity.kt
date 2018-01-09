c4ea864586023133c41885b7036594d0503acae7
Hzxr_ <13277250082@163.com>
2018-01-09 21:14:45 +0800

    feature:add BaseActivity to be the super Activity

---
+++ app/src/main/java/com/example/hzxr/openfiredemo/UI/BaseActivity.kt
@@ -0,0 +1,17 @@
+package com.example.hzxr.openfiredemo.UI
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.Window
+
+/**
+ * Created by Hzxr on 2018/1/9.
+ */
+class BaseActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+    }
+
+}
