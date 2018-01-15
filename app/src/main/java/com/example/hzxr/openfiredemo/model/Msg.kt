649ad30d91b7919baedfff09ffd7fea37cedfc7e
Hzxr_ <13277250082@163.com>
2018-01-15 16:39:56 +0800

    feature: refactor Msg Model

--- app/src/main/java/com/example/hzxr/openfiredemo/model/Msg.kt
+++ app/src/main/java/com/example/hzxr/openfiredemo/model/Msg.kt
@@ -3,7 +3,10 @@ package com.example.hzxr.openfiredemo.model
 /**
  * Created by Hzxr on 2018/1/8.
  */
-class Msg(var content: String, var userId: String, var type: String){
+class Msg(var content: String, var userId: String, var type: String) {
+
+    constructor(content: String, userId: String, type: String, filePath: String): this(content, userId, type)
+
     override fun toString(): String {
         return "UserId: " + userId + " content: " + content + " type: " + type
     }
